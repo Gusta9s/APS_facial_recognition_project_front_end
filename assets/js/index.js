0c118672aa135e75eeab62806ec4fb1fb0341118
Gustavo de Almeida Pacheco001 <gustavoapacheco@yahoo.com.br>
2024-09-09 19:09:49 -0300

    feat(index.js): Implementacao de funcionalidade de seguimentacao e reconhecimento de rosto
    Implementada a funcionalidade de seguimentacao atraves do canvas e do metodo detectAllFaces e aplicacao de uma acuracia de acerto das imagens capturadas, implementada a chamada ao sistema de camera com base no seu hardware.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,59 @@
+/**
+ *  Uma descricao rapida do que se trata cada uma das bibliotecas consumidas dentro da promisse:
+ *  - tinyFaceDetector: E a biblioteca responsavel por realizar o procedimento de seguimentacao de uma imagem e atribuir as classes de objetos/rosto para prcessamento.
+ *  - faceLandmark68Net: E a biblioteca responsavel por realizar dentro da classe de objeto, a cricao dos pontos de identificacao, que o modelo de aprendizado supervisionado ira consumir para definir os pontos em comum entre todas imagens de entrada, do que sera comparado no futuro pelas previsioes para autentificacao e habilitacao de acesso do usuario.
+ *  - faceRecognitionNet: E responsavel por atraves da matris de pixels vindo de entrada, buscar nas bases de dados da aplicacao de quem se trata aquele usuario e buscar seus dados pessoais.
+ *  - faceExpressionNet: E a biblioteca responsavel por dectecar emocoes e expressoes vindas do usuarios e resgistra-las.
+ *  - ageGenderNet: E uma biblioteca responsavel por registrar uma tentativa de melhor esforco com base em um tempo, qual seria a minha idade e meu sexo (masculino, feminino). Atraves de um modelo treinado com dados reais de pessoas e suas fotos de uma aproximacao do que seria a imagem vinda como entrada e gerar uma previsao disto.
+ *  - ssdMobilenetv1: E a biblioteca responsavel por fazer a estilizacao do faceLandmark68Net e aplicar os quadrados de deteccao e etc.
+ */
+
 const camera = document.getElementById('camera')
 
-navigator.mediaDevices.enumerateDevices().then(devices => {    
-    if (Array.isArray(devices)) {
-        devices.forEach(device => {
-            if (device.kind === 'videoinput') {
-                if (device.label.includes('')){
-                    navigator.getUserMedia(
-                        { video: {
-                            deviceId: device.deviceId
-                        }},
-                        stream => camera.srcObject = stream,
-                        error => console.error(error)
-                    )
+const startVideo = () => {
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+        if (Array.isArray(devices)) {
+            devices.forEach(device => {
+                if (device.kind === 'videoinput') {
+                    if (device.label.includes('')) {
+                        navigator.getUserMedia(
+                            {
+                                video: {
+                                    deviceId: device.deviceId
+                                }
+                            },
+                            stream => camera.srcObject = stream,
+                            error => console.error(error)
+                        )
+                    }
                 }
-            }
-        })
+            })
+        }
+    })
+}
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/assets/lib/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/assets/lib/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/assets/lib/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/assets/lib/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/assets/lib/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/assets/lib/models')
+]).then(startVideo)
+
+camera.addEventListener('play', async () => {
+    const canvas = faceapi.createCanvasFromMedia(camera)
+    const canvasSize = {
+        width: camera.videoWidth,
+        height: camera.videoHeight
     }
+    faceapi.matchDimensions(canvas, canvasSize)
+    document.body.appendChild(canvas)
+    setInterval(async () => {
+        const detecoes = await faceapi.detectAllFaces(camera, new faceapi.TinyFaceDetectorOptions())
+        const resizedDetections = faceapi.resizeResults(detecoes, canvasSize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+
+    }, 100)
 })
